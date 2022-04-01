b49faa36127782cbf4c5601266621476adcbf3f4
Just An Introvert :) <91733049+kagoyaa@users.noreply.github.com>
2022-04-01 12:18:41 +0700

    Add files via upload

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node . -- db 'https://cloud.mongodb.com/v2/6246687e4433f83515954aeb#clusters'
+worker: npm i -g pm2 && pm2 start index.js && pm2 save && pm2 logs
--- index.js
+++ index.js
@@ -161,7 +161,7 @@ async function startHisoka() {
 	for (let i of kon) {
 	    list.push({
 	    	displayName: await hisoka.getName(i + '@s.whatsapp.net'),
-	    	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:${await hisoka.getName(i + '@s.whatsapp.net')}\nFN:${await hisoka.getName(i + '@s.whatsapp.net')}\nitem1.TEL;waid=${i}:${i}\nitem1.X-ABLabel:Ponsel\nitem2.EMAIL;type=INTERNET:okeae2410@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://instagram.com/cak_haho\nitem3.X-ABLabel:Instagram\nitem4.ADR:;;Indonesia;;;;\nitem4.X-ABLabel:Region\nEND:VCARD`
+	    	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:${await hisoka.getName(i + '@s.whatsapp.net')}\nFN:${await hisoka.getName(i + '@s.whatsapp.net')}\nitem1.TEL;waid=${i}:${i}\nitem1.X-ABLabel:Ponsel\nitem2.EMAIL;type=INTERNET:radityaFX12@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://kagoyaa-api.herokuapp.com/\nitem3.X-ABLabel:Youtube\nitem4.ADR:;;Indonesia;;;;\nitem4.X-ABLabel:Region\nEND:VCARD`
 	    })
 	}
 	hisoka.sendMessage(jid, { contacts: { displayName: `${list.length} Kontak`, contacts: list }, ...opts }, { quoted })
